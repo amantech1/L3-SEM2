afd20970530460dbff6020e671c49d0d98b52082
Aman Tech <amanmahato022@gmail.com>
2026-06-07 23:16:00 +0545

    Delete Week_1_2 directory

--- Week_1_2/Q3_Even_Odd.py
+++
@@ -1,9 +0,0 @@
-#Take a number input from the user and display whether the number is even or odd
-
-a = int(input('Enter a number: '))
-
-#Check for Even or Odd
-if a%2 == 0:
-    print('The given number is Even')
-else:
-    print('The given number is Odd')
